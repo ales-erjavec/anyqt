d4e567e97e76ca5cfd5bd75bdb904230434ea414
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2016-12-12 16:36:05 +0100

    Release version 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import io
 from setuptools import setup, find_packages
 
 NAME = "AnyQt"
-VERSION = "0.0.7.dev0"
+VERSION = "0.0.7"
 AUTHOR = "Aleš Erjavec"
 AUTHOR_EMAIL = "ales.erjavec@fri.uni-lj.si"
 URL = "https://github.com/ales-erjavec/anyqt"
