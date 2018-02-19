51f1b6fdc3a60a7bdec6d727a737cb99bf8e7f7a
Julien Schueller <schueller@phimeca.com>
2018-02-19 22:48:08 +0100

    Typo

--- AnyQt/QtOpenGL.py
+++ AnyQt/QtOpenGL.py
@@ -5,6 +5,6 @@ if _api.USED_API == _api.QT_API_PYQT5:
 elif _api.USED_API == _api.QT_API_PYQT4:
     from PyQt4.QtOpenGL import *
 elif _api.USED_API == _api.QT_API_PYSIDE:
-    from PySide.QtOpenGl import *
+    from PySide.QtOpenGL import *
 
 
