11c507c3ff5dd566bbd0485d036017a9af79b4a7
Julien Schueller <schueller@phimeca.com>
2018-02-26 10:56:37 +0100

    Fix QtQuick

--- AnyQt/QtQuick.py
+++ AnyQt/QtQuick.py
@@ -2,7 +2,7 @@ from . import _api
 
 if _api.USED_API == _api.QT_API_PYQT5:
     from PyQt5.QtQuick import *
-if _api.USED_API == _api.QT_API_PYSIDE2:
+elif _api.USED_API == _api.QT_API_PYSIDE2:
     from PySide2.QtQuick import *
 else:
     raise ImportError("No module named 'QtQuick' in the selected "
