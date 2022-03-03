720c09c3886ebb1a409173b6b3f41e604c3e1511
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2022-03-03 13:33:05 +0100

    Update imports of QWebEngineCore components for Qt6

--- AnyQt/QtWebEngineCore.py
+++ AnyQt/QtWebEngineCore.py
@@ -4,6 +4,20 @@ if _api.USED_API == _api.QT_API_PYQT6:
     from PyQt6.QtWebEngineCore import *
 elif _api.USED_API == _api.QT_API_PYQT5:
     from PyQt5.QtWebEngineCore import *
+    try:
+        from PyQt5.QtWebEngineWidgets import (
+            QWebEngineHistory,
+            QWebEngineProfile,
+            QWebEngineScript,
+            QWebEngineScriptCollection,
+            QWebEngineClientCertificateSelection,
+            QWebEngineSettings,
+            QWebEngineFullScreenRequest,
+        )
+    except ImportError:
+        pass
+
+
 else:
     raise ImportError("No module named 'QtWebEngineCore' in the selected "
                       "Qt api ({})".format(_api.USED_API))
--- AnyQt/QtWebEngineWidgets.py
+++ AnyQt/QtWebEngineWidgets.py
@@ -2,6 +2,15 @@ from . import _api
 
 if _api.USED_API == _api.QT_API_PYQT6:
     from PyQt6.QtWebEngineWidgets import *
+    from PyQt6.QtWebEngineCore import (
+        QWebEngineHistory,
+        QWebEngineProfile,
+        QWebEngineScript,
+        QWebEngineScriptCollection,
+        QWebEngineClientCertificateSelection,
+        QWebEngineSettings,
+        QWebEngineFullScreenRequest,
+    )
 elif _api.USED_API == _api.QT_API_PYQT5:
     from PyQt5.QtWebEngineWidgets import *
 elif _api.USED_API == _api.QT_API_PYSIDE2:
