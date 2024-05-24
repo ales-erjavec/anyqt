73811ddeec9c929accb75266051588f4aec980a3
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2024-05-24 13:15:54 +0200

    Expose QtRemoteObjects module

---
+++ AnyQt/QtRemoteObjects.py
@@ -0,0 +1,16 @@
+from . import _api
+
+if _api.USED_API == _api.QT_API_PYQT6:
+    from PyQt6.QtRemoteObjects import *
+elif _api.USED_API == _api.QT_API_PYQT5:
+    from PyQt5.QtRemoteObjects import *
+elif _api.USED_API == _api.QT_API_PYQT4:
+    from PyQt4.QtRemoteObjects import *
+elif _api.USED_API == _api.QT_API_PYSIDE:
+    from PySide.QtRemoteObjects import *
+elif _api.USED_API == _api.QT_API_PYSIDE2:
+    from PySide2.QtRemoteObjects import *
+elif _api.USED_API == _api.QT_API_PYSIDE6:
+    from PySide6.QtRemoteObjects import *
+
+_api.apply_global_fixes(globals())
